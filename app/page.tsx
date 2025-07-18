3c24600d2af3f45de03c89dd0a34fabbb28b5e89
kabileshkumar <kabileshkumar2211@gmail.com>
2025-07-18 23:53:49 +0530

    Home page UI Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,49 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionList from "@/components/CompanionList";
+import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
 import React from "react";
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline">Welcome to my Saas Product</h1>
-      <Button>Let&apos;s get Started</Button>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="456"
+          name="Counter the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verbal the Vocablary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:lg:w-full"
+        />
+        <Cta />
+      </section>
     </div>
   );
 };
